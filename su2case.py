fb30cec513258d59009f55aef2519ae85a99053d
stephan <stephansmit@hotmail.com>
2020-03-16 12:26:47 +0100

    added the 3d turbine

--- su2case.py
+++ su2case.py
@@ -133,6 +133,7 @@ class SU2TriogenTurbineFOSOCase_WInletConditions(SU2FOSOCase):
        cfg_so.set_rotational_speed(self.rotation_speed)
        cfg_so.set_second_order()
        self.cfgs = [cfg_fo, cfg_so, cfg_so]
+
 class SU2TriogenTurbinePPCase(SU2Case):
     def __init__(self, cname, work_dir, image_dir, mesh_dir):
        self.fname = 'turbine'
@@ -179,3 +180,34 @@ class SU2TriogenStatorFOSOCase(SU2FOSOCase):
        cfg_so.set_second_order()
        self.cfgs = [cfg_fo, cfg_so, cfg_so]
 
+class SU2TriogenTurbine3D_FOCase(SU2Case):
+    def __init__(self, cname, work_dir, image_dir, mesh_dir, rotation_speed, nblades, total_temperature, total_pressure):
+       self.rotation_speed = rotation_speed
+       self.nblades = nblades
+       self.total_temperature = total_temperature
+       self.total_pressure = total_pressure
+       self.fname = 'turbine'
+       SU2Case.__init__(self, cname, work_dir, image_dir, mesh_dir)
+
+
+    def run(self,ncores):
+        for i, cmd in enumerate(self.cmds):
+           sim = SU2Simulation(self.case_dir, self.image_dir, self.mesh_dir)
+           sim.image_url = 'shub://stephansmit/su2_containers:blackbird_v7.0.2'
+           sim.run(cmd, ncores, self.cfgs[i], self.logs[i])
+
+   def set_cfgs(self):
+       cfg_fo = SU2MultiConfigFile(self.fname+'_fo.cfg', self.case_dir, self.cfg_dir)
+       cfg_fo.initialize('turbine.template.cfg')
+       cfg_fo.content['RESTART_SOL']="NO"
+       cfg_fo.content['OUTER_ITER']=8000
+       cfg_fo.content['SOLUTION_FLOW_FILENAME']='turbine_fo.dat'
+       cfg_fo.content['RESTART_FLOW_FILENAME']='turbine_fo.dat'
+       cfg_fo.content['VOLUME_FILENAME']='flow_fo'
+       cfg_fo.content['MARKER_GILES']="(inflow, TOTAL_CONDITIONS_PT, "+str(self.total_pressure)+","+str(self.total_temperature)+", 1.0, 0.0, 0.0, 0.9, 0.0, outmix, MIXING_OUT, 0.0, 0.0, 0.0, 0.0, 0.0, 0.95, 0.0, inmix, MIXING_IN, 0.0, 0.0, 0.0, 0.0, 0.0, 0.95,0.0, outflow, STATIC_PRESSURE, 20000, 0.0, 0.0, 0.0, 0.0 , 1.0,0.0)"
+       cfg_fo.content['CFL_NUMBER']=1.0
+       cfg_fo.content['CONV_FILENAME']='history_fo'
+       cfg_fo.set_number_blades(self.nblades)
+       cfg_fo.set_rotational_speed(self.rotation_speed)
+       cfg_fo.set_first_order()
+       self.cfgs = [cfg_fo, cfg_fo]
